e7f241992d3bc8d70008344eff19ea54ac6000c9
Arjun Manjunath <arjunmanjunath80@gmail.com>
2024-09-09 19:01:23 +0530

    Update product.py

--- api/routes/product.py
+++ api/routes/product.py
@@ -2,7 +2,7 @@ from flask import request, jsonify
 from flask_restful import Resource
 from api.models import db, Category, Product, ProductImage, CatProd
 from sqlalchemy.exc import IntegrityError
-from . import api
+from api.routes import api
 
 # Resource for managing a single product
 class ProductResource(Resource):
