93d9dcd945819266780c2514cf429c76f1cd93d1
Sanin K <146609225+sanin7k@users.noreply.github.com>
2024-09-09 11:02:32 +0530

    Update config.py

--- api/config.py
+++ api/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost/webauctiondb'
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
